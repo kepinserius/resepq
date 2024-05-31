fd7e1c6addc4857329e59f3af1856dff9d0f16e7
Bagus Rama M <68902437+Bagus342@users.noreply.github.com>
2024-05-31 18:50:05 +0700

    Update index.blade.php
    change href login

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,7 +38,7 @@
    <div class="header_section">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="logo">
-              <a href="/login">
+              <a href="/auth/login">
                   <img src="logoanjay.png" style="width: 130px; height: auto;">
               </a>
           </div>
